e23ec8573f430f564a9d6c9c2c7e365349d9b794
dlobser <dal230@nyu.edu>
2018-06-20 23:36:59 -0400

    made simple stuff

---
+++ Assets/ITParticles/Script/ParticleRenderSimple.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRenderSimple : MonoBehaviour
+{
+    public ParticleSolverSimple Solver;
+
+    ComputeBuffer meshBuffer;
+
+    //public int ParticleCount;
+    public Mesh PerParticleMesh;
+
+    MeshFilter meshFilter;
+    Material mat;
+    private Mesh dummyMesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mat = GetComponent<MeshRenderer>().sharedMaterial;
+
+        CreateDummyMesh();
+
+        CreateMeshBuffer();
+    }
+
+    private void Update()
+    {
+        UpdateMaterialProperties();
+    }
+
+    private void OnDestroy()
+    {
+        if (meshBuffer != null)
+            meshBuffer.Release();
+    }
+
+    private void UpdateMaterialProperties()
+    {
+        mat.SetBuffer("particleBuffer", Solver.particleBuffer);
+        mat.SetBuffer("meshDataBuffer", meshBuffer);
+        mat.SetInt("MeshIndexCount", PerParticleMesh.triangles.Length);
+    }
+
+    private void CreateMeshBuffer()
+    {
+        var meshDatarr = new MeshData[PerParticleMesh.triangles.Length];
+
+        for (int i = 0; i < PerParticleMesh.vertices.Length; i++)
+            meshDatarr[i].vert = PerParticleMesh.vertices[i];
+
+        for (int i = 0; i < PerParticleMesh.uv.Length; i++)
+            meshDatarr[i].uv = PerParticleMesh.uv[i];
+
+        for (int i = 0; i < PerParticleMesh.triangles.Length; i++)
+            meshDatarr[i].index = PerParticleMesh.triangles[i];
+
+        for (int i = 0; i < PerParticleMesh.normals.Length; i++)
+            meshDatarr[i].normal = PerParticleMesh.normals[i];
+
+        meshBuffer = new ComputeBuffer(meshDatarr.Length, MeshData.stride);
+        meshBuffer.SetData(meshDatarr);
+    }
+
+    private void CreateDummyMesh()
+    {
+        int vCount = PerParticleMesh.vertexCount * Solver.ParticleCount;
+
+        var verts = new Vector3[vCount];
+        var uvs = new Vector2[vCount];
+        var ind = new int[vCount];
+
+        dummyMesh = new Mesh();
+        for (int i = 0; i < vCount; i++)
+        {
+            verts[i] = Vector3.zero;
+            uvs[i] = Vector2.zero;
+            ind[i] = i;
+        }
+
+        var topology = PerParticleMesh.GetTopology(0);
+
+        dummyMesh.name = "Dummy Mesh with " + vCount + " Verts";
+        dummyMesh.vertices = verts;
+        dummyMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        dummyMesh.SetIndices(ind, MeshTopology.Triangles, 0);
+        dummyMesh.bounds = new Bounds(Vector3.zero, new Vector3(1000, 1000, 1000));
+
+        meshFilter.mesh = dummyMesh;
+    }
+}
---
+++ Assets/ITParticles/Script/ParticleSolverSimple.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ParticleSolverSimple : MonoBehaviour
+{
+
+    public int ParticleCount = 5000;
+
+    public ComputeShader Kernels;
+    public ComputeBuffer particleBuffer;
+    public ComputeBuffer forceBuffer;
+
+    public Vector3 ConstantVelocity = Vector3.zero;
+    public Vector3 center;
+    public float power;
+
+    int UpdateParticleIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateParticleIndex = Kernels.FindKernel("UpdateParticles");
+        CreateInitialPositions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Kernels.SetBuffer(UpdateParticleIndex, "particleBuffer", particleBuffer);
+        Kernels.SetFloat("deltaTime", Time.deltaTime);
+        Kernels.SetFloat("power", power);
+        Kernels.SetVector("center", center);
+
+        Kernels.SetVector("constantVelocity", ConstantVelocity);
+        Kernels.Dispatch(UpdateParticleIndex, ParticleCount, 1, 1);
+    }
+
+    private void OnDestroy()
+    {
+        if (particleBuffer != null)
+            particleBuffer.Release();
+        if (forceBuffer != null)
+            forceBuffer.Release();
+    }
+    
+    private void CreateInitialPositions()
+    {
+        Particle[] particleArray = new Particle[ParticleCount];
+        for (int i = 0; i < ParticleCount; ++i)
+        {
+            particleArray[i].position = 2 * Random.onUnitSphere;
+            particleArray[i].velocity.x = 0;
+            particleArray[i].velocity.y = 0;
+            particleArray[i].velocity.z = 0;
+        }
+
+        particleBuffer = new ComputeBuffer(ParticleCount, Particle.stride);
+        particleBuffer.SetData(particleArray);
+    }
+
+}
